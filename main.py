c5436ecb934178e8eaa6cc7105977f1999299fcd
david.delrio <david.delrio@congreso.es>
2025-03-24 11:42:37 +0100

    Add env variables for listening host and port.

--- main.py
+++ main.py
@@ -67,4 +67,6 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    host = os.environ.get('SOLAR_LIBRE_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('SOLAR_LIBRE_API_PORT', 8000))
+    uvicorn.run(app, host=host, port=port)
