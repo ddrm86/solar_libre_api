db31da1cc99034ac0d56c4d90a9274d57cb50a45
David del Río Medina <ddrm86@gmail.com>
2025-04-04 15:21:19 +0200

    Add working get endpoint.

--- pvgis/pvgis.py
+++ pvgis/pvgis.py
@@ -1,5 +1,8 @@
+import requests
+from typing import Annotated
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
-from fastapi import APIRouter
+
 
 class PVGISRequest(BaseModel):
     latitude: float
@@ -10,13 +13,18 @@ class PVGISRequest(BaseModel):
     azimuth: int
 
 
-class PVGISResponse(BaseModel):
-    pass
-
 router = APIRouter(
     prefix="/pvgis",
 )
 
-@router.get('/', response_model=PVGISResponse)
-def get_pvgis_data(request: PVGISRequest):
-    return PVGISResponse()
+@router.get('/')
+def get_pvgis_data(req_params: Annotated[PVGISRequest, Query()]):
+    base_url = 'https://re.jrc.ec.europa.eu/api/v5_2/PVcalc'
+    params = {'lat': req_params.latitude, 'lon': req_params.longitude,
+              'peakpower': req_params.peak_power, 'loss': req_params.loss,
+              'angle': req_params.angle, 'aspect': req_params.azimuth, 'outputformat': 'json'}
+    request = requests.get(base_url, params=params, timeout=3)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        return {'error': 'Error al obtener datos de PVGIS', 'status_code': request.status_code}
