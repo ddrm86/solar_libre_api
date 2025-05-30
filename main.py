5e919cf752ecba254d8d079fe48ac30a9f9846f6
David del Río Medina <ddrm86@gmail.com>
2025-05-30 18:02:04 +0200

    Add all basic classes and endpoints for inverter setups.
    - Without their associated MPPT setups.

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from inventory import panels, monophase_inverters
 from pvgis import pvgis
 from project_info import project_info
 from consumption import energy_consumption
-from installation import solar_arrays
+from installation import solar_arrays, inverter_setups
 import db
 
 
@@ -42,6 +42,7 @@ app.include_router(pvgis.router)
 app.include_router(project_info.router)
 app.include_router(energy_consumption.router)
 app.include_router(solar_arrays.router)
+app.include_router(inverter_setups.router)
 
 
 DEFAULT_CORS_ORIGINS = '["http://localhost:8080", "http://localhost:5173"]'
